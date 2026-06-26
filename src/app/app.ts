86a4b9a67b72d9b002d81b6d4015756f38837fcd
Mohamed Gassama <gassma803@gmail.com>
2026-06-26 09:52:20 +0000

    chore(frontend): initialiser le projet Angular

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('G-School Web');
+  protected readonly subtitle = signal('Plateforme de formation en ligne');
+}
